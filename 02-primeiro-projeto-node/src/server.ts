67b670acf73c66aab589d2560d01104f7e388257
douglas <douglasantana007@gmail.com>
2020-04-18 23:20:12 -0300

    [ADD]:rocket: Appointment With Pattern Repository.
    Adding project for create appointmnet with patterns Repository and Service.

--- 02-primeiro-projeto-node/src/server.ts
+++ 02-primeiro-projeto-node/src/server.ts
@@ -8,6 +8,4 @@ app.use(routes);
 
 app.get('/', (req, res) => res.json({ message: ' Hello GoStack !' }));
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-});
+app.listen(3333);
